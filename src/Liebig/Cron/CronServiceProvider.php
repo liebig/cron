2c49080215f81225a71b9f2c0f287f29bf8f6c26
Marc Liebig <marc.liebig@gmx.de>
2013-06-27 13:39:55 +0200

    Initial commit

---
+++ src/Liebig/Cron/CronServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Liebig\Cron;
+
+use Illuminate\Support\ServiceProvider;
+
+class CronServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('liebig/cron');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
