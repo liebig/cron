9111fd3e9dbb53f8331d2c0bdca754fb2cf4310f
Marc Liebig <marc.liebig@gmx.de>
2013-06-27 16:29:04 +0200

    Adding database migration files

---
+++ src/migrations/2013_06_27_143953_create_cronmanager_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCronmanagerTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('cron_manager', function($table) {
+                    $table->increments('id');
+                    $table->dateTime('rundate');
+                    $table->float('runtime');
+                    $table->integer('errors');
+                    $table->timestamps();
+                });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('cron_manager');
+    }
+
+}
---
+++ src/migrations/2013_06_27_144035_create_cronerror_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCronerrorTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('cron_error', function($table) {
+                    $table->increments('id');
+                    $table->string('name');
+                    $table->text('return');
+                    $table->float('runtime');
+                    $table->dateTime('rundate');
+                    $table->integer('cron_manager_id');
+                    $table->timestamps();
+                });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('cron_error');
+    }
+
+}
