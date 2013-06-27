435897bca9c81c427b8976a8651be5797925e54c
Marc Liebig <marc.liebig@gmx.de>
2013-06-27 14:03:27 +0200

    Adding example for cron

--- src/Liebig/Cron/CronServiceProvider.php
+++ src/Liebig/Cron/CronServiceProvider.php
@@ -1,44 +1,56 @@
-<?php namespace Liebig\Cron;
+<?php
+
+namespace Liebig\Cron;
 
 use Illuminate\Support\ServiceProvider;
 
 class CronServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('liebig/cron');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+        $this->package('liebig/cron');
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+        \Liebig\Cron\Cron::add('test', '* * * * *', function() {
+                    echo '<h1>cron runned!</h1>';
+                    return true;
+                });
+
+        \Liebig\Cron\Cron::add('test', '*/2 * * * *', function() {
+                    echo '<h1>cron not runned!</h1>';
+                    return false;
+                });
+
+        var_dump(\Liebig\Cron\cron::run());
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return array();
+    }
 
 }
