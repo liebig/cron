f9d045034cdf44bdeb23bc27d4139f2f58f08fd9
Marc Liebig <marc.liebig@gmx.de>
2013-11-12 14:22:04 +0100

    Adding configuration for enabling Laravels build in logging support - standard value is true

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,9 @@ return array(
     // Interval defines the time in minutes between two run method calls - in other words, the time between the cron job route will be called
     'runInterval' => 1,
     
+    // Should the Laravel build in logger handle logging
+    'laravelLogging' => true,
+    
     // Enable or disable database logging
     'databaseLogging' => true,
     
