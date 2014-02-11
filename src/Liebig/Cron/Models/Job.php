fc6ad29bde43ef743385e1054834eaf035601b22
Marc Liebig <marc.liebig@gmx.de>
2014-02-11 09:29:40 +0100

    Remove $fillable attribute because mass assignment is not required

--- src/Liebig/Cron/Models/Job.php
+++ src/Liebig/Cron/Models/Job.php
@@ -6,7 +6,6 @@ class Job extends \Eloquent{
     
     protected $table = 'cron_job';
     public $timestamps = false;
-    protected $fillable = array('name', 'return', 'runtime');
     
     public function manager() {
         return $this->belongsTo('\Liebig\Cron\Models\Manager', 'cron_manager_id');
--- src/Liebig/Cron/Models/Manager.php
+++ src/Liebig/Cron/Models/Manager.php
@@ -6,7 +6,6 @@ class Manager extends \Eloquent{
     
     protected $table = 'cron_manager';
     public $timestamps = false;
-    protected $fillable = array('rundate', 'runtime');
     
     public function cronJobs() {
         return $this->hasMany('\Liebig\Cron\Models\Job', 'cron_manager_id');
