8808fd8adb480c166dc03416f7a15b51863bdf19
Marc Liebig <marc.liebig@gmx.de>
2014-05-06 11:44:42 +0200

    Register the Cron commands

--- src/Liebig/Cron/CronServiceProvider.php
+++ src/Liebig/Cron/CronServiceProvider.php
@@ -29,13 +29,23 @@ class CronServiceProvider extends ServiceProvider {
      */
     public function register() {
         $this->app['cron'] = $this->app->share(function($app) {
-            return  new Cron;
-        });
-        
+                    return new Cron;
+                });
+
         $this->app->booting(function() {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-            $loader->alias('Cron', 'Liebig\Cron\Facades\Cron');
-        });
+                    $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+                    $loader->alias('Cron', 'Liebig\Cron\Facades\Cron');
+                });
+
+        $this->app['cron::command.run'] = $this->app->share(function($app) {
+                    return new RunCommand;
+                });
+        $this->commands('cron::command.run');
+
+        $this->app['cron::command.list'] = $this->app->share(function($app) {
+                    return new ListCommand;
+                });
+        $this->commands('cron::command.list');
     }
 
     /**
