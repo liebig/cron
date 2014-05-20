907331bbfee7504af302486190835e0577d0ccdb
Marc Liebig <marc.liebig@gmx.de>
2014-05-20 14:20:07 +0200

    Fixing comment

--- src/config/config.php
+++ src/config/config.php
@@ -2,7 +2,7 @@
 
 return array(
     
-    // Interval defines the time in minutes between two run method calls - in other words, the time between the cron job route will be called
+    // Interval defines the time in minutes between two run method calls - in other words, the time between the Cron route or command will be called
     'runInterval' => 1,
     
     // Should the Laravel build in logger handle logging
@@ -14,7 +14,7 @@ return array(
     // Enable or disable logging error jobs only
     'logOnlyErrorJobsToDatabase' => false,
     
-    // Delte old database entries after how many hours
+    // Delte old database entries after how many hours - if this value is set to 0, no entries will be deleted
     'deleteDatabaseEntriesAfter' => 240,
     
     // Prevent job overlapping - if Cron is still running it could not be started a second time
